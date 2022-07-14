35698793487d3eaca10719c848f23da96e205a85
capdiem <capdiem@live.com>
2022-07-15 00:48:04 +0800

    🆕 feat: auto scroll

--- src/MASA.OfficialWebsite.Shared/Pages/Home.razor.cs
+++ src/MASA.OfficialWebsite.Shared/Pages/Home.razor.cs
@@ -1,7 +1,13 @@
-﻿namespace MASA.OfficialWebsite.Shared.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
-public partial class Home
+namespace MASA.OfficialWebsite.Shared.Pages;
+
+public partial class Home : IAsyncDisposable
 {
+    [Inject]
+    private IJSRuntime Js { get; set; } = null!;
+
     private static readonly List<Activity> Activities = new()
     {
         new Activity("MASA Stack 社区例会", "MASA Stack最新进度汇报",
@@ -14,4 +20,27 @@ public partial class Home
             "_content/MASA.OfficialWebsite.Shared/images/activity1.png",
             DateTime.Now, 1, ActivityType.LookBack),
     };
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await Js.InvokeVoidAsync("MasaOfficialWebsite.addWindowsScrollEvent");
+            StateHasChanged();
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        try
+        {
+            await Js.InvokeVoidAsync("MasaOfficialWebsite.removeWindowsScrollEvent");
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+    }
 }
---
+++ src/MASA.OfficialWebsite.Shared/wwwroot/js/shared.js
@@ -0,0 +1,50 @@
+﻿window.MasaOfficialWebsite = {}
+
+window.MasaOfficialWebsite.addWindowsScrollEvent = () => {
+  let timer
+  let timeout
+
+  const listener = (e) => {
+    console.log(Date.now())
+    const innerHeight = window.innerHeight || document.body.clientHeight
+    const offsetY = document.body.scrollTop || document.documentElement.scrollTop;
+
+    let targetTop = 0;
+
+    if (offsetY < innerHeight / 2) {
+      targetTop = 0
+    } else if (offsetY >= innerHeight / 2 && offsetY < innerHeight * 1.5) {
+      targetTop = innerHeight
+    } else if (offsetY >= innerHeight * 1.5 && offsetY < innerHeight * 2) {
+      targetTop = innerHeight * 2
+    } else {
+      return
+    }
+
+    const c = offsetY - targetTop
+    const startTime = +new Date();
+    const duration = 500;
+
+    cancelAnimationFrame(timer)
+
+    timer = requestAnimationFrame(function f() {
+      const time = duration - Math.max(0, startTime - (+new Date()) + duration);
+      document.body.scrollTop = document.documentElement.scrollTop = time * (-c) / duration + offsetY;
+      timer = requestAnimationFrame(f)
+
+      if (time === duration) {
+        cancelAnimationFrame(timer)
+      }
+    })
+  };
+
+  window.addEventListener("scroll", e => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => listener(e), 500);
+  })
+}
+
+window.MasaOfficialWebsite.removeWindowsScrollEvent = () => {
+  // TODO: 需要把listener缓存起来才能删除
+  window.removeEventListener("scroll")
+}
