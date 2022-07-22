2bea1da546664ec9efd6139ebea797afd5386b46
yesunda <yesunda@masa.lonsid.cn>
2022-07-22 09:50:07 +0800

    feat: blazor

---
+++ src/MASA.OfficialWebsite.Shared/Pages/Blazor.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASA.OfficialWebsite.Shared.Pages
+{
+    public partial class Blazor
+    {
+        [Inject]
+        private IJSRuntime Js { get; set; } = null!;
+
+        [CascadingParameter(Name = "IsMobile")]
+        private bool IsMobile { get; set; }
+
+        private async Task ScrollToNext()
+        {
+            await Js.InvokeVoidAsync("MasaOfficialWebsite.scrollToNext");
+        }
+    }
+}
