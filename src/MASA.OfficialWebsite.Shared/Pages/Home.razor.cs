dd4f3a81e4b924d84cde3a9a8ee636be2998d831
capdiem <capdiem@live.com>
2022-07-25 14:28:06 +0800

    ♻ refactor: remove unused code and format the code

--- src/MASA.OfficialWebsite.Shared/Pages/Home.razor.cs
+++ src/MASA.OfficialWebsite.Shared/Pages/Home.razor.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
-
-namespace MASA.OfficialWebsite.Shared.Pages;
+﻿namespace MASA.OfficialWebsite.Shared.Pages;
 
 public partial class Home : IAsyncDisposable
 {
--- src/MASA.OfficialWebsite.Shared/_Imports.cs
+++ src/MASA.OfficialWebsite.Shared/_Imports.cs
@@ -3,3 +3,5 @@
 global using MASA.OfficialWebsite.Shared.Shared;
 global using MASA.OfficialWebsite.Shared.Models;
 global using BlazorComponent;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.JSInterop;
