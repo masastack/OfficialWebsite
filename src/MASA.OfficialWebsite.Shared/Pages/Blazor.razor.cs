c6fd9e1572a9aeb346a24fe600f092d9429a3c9f
ysd0002 <109396190+ysd0002@users.noreply.github.com>
2022-08-09 17:32:31 +0800

    fix (#20)

--- src/MASA.OfficialWebsite.Shared/Pages/Blazor.razor.cs
+++ src/MASA.OfficialWebsite.Shared/Pages/Blazor.razor.cs
@@ -3,5 +3,9 @@
     public partial class Blazor : AutoScrollComponentBase
     {
         protected override int? Page => 1;
+
+        private StringNumber _carouselValue = 0;
+
+        private StringNumber BannerMaxSize => IsMobile ? 375 : 874;
     }
 }
