d1e6bd0be40f45fc50e970056748f03e0d689bba
capdiem <capdiem@live.com>
2022-08-09 13:19:43 +0800

    🆕 feat(home): add touch events to banners

--- src/MASA.OfficialWebsite.Shared/Pages/Home.razor.cs
+++ src/MASA.OfficialWebsite.Shared/Pages/Home.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace MASA.OfficialWebsite.Shared.Pages;
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace MASA.OfficialWebsite.Shared.Pages;
 
 public partial class Home : AutoScrollComponentBase
 {
@@ -21,7 +23,58 @@ public partial class Home : AutoScrollComponentBase
         new MenuableTitleItem("最新活动", "最新社区活动预告与往期回顾", "#activity-content"),
     };
 
+    private double _startX;
+    private double _deltaX;
+    private double _offsetX;
+    private string? _direction;
+
     private StringNumber _carouselValue = 0;
 
     private StringNumber BannerMaxSize => IsMobile ? 375 : 874;
+
+    private void OnTouchstart(TouchEventArgs args)
+    {
+        ResetTouchStatus();
+        _startX = args.Touches[0].ClientX;
+    }
+
+    private void OnTouchmove(TouchEventArgs args)
+    {
+        var touch = args.Touches[0];
+        _deltaX = touch.ClientX < 0 ? 0 : touch.ClientX - _startX;
+        _offsetX = Math.Abs(_deltaX);
+
+        const int lockDirectionDistance = 10;
+        if (string.IsNullOrEmpty(_direction) || _offsetX < lockDirectionDistance)
+        {
+            _direction = _deltaX < 0 ? "left2right" : "right2left";
+        }
+    }
+
+    private void OnTouchend(TouchEventArgs args)
+    {
+        switch (_direction)
+        {
+            case "left2right":
+            {
+                var nextValue = _carouselValue.ToInt32() + 1;
+                _carouselValue = nextValue > 2 ? 0 : nextValue;
+                break;
+            }
+            case "right2left":
+            {
+                var prevValue = _carouselValue.ToInt32() - 1;
+                _carouselValue = prevValue < 0 ? 2 : prevValue;
+                break;
+            }
+        }
+    }
+
+    private void ResetTouchStatus()
+    {
+        _direction = "";
+        _startX = 0;
+        _deltaX = 0;
+        _offsetX = 0;
+    }
 }
